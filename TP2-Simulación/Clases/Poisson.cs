1fa6d7e46b85b10ce8af9c8f3d928e42fd83b8c5
GuadiAguilera <54043406+GuadiAguilera@users.noreply.github.com>
2023-04-12 18:11:57 -0300

    guadi-1

---
+++ TP2-Simulación/Clases/Poisson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Simulación.Clases
+{
+    internal class Poisson
+    {
+        private int lambda;
+        private int datos;
+
+
+        double u;
+        double p;
+
+        private Random random = new Random();
+    }
+
+
+    public void generarValorX()
+    {
+        
+    }
+}
