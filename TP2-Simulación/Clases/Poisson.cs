12e8730a3aef137d8a240e99a8972d4b1b86b82a
GuadiAguilera <54043406+GuadiAguilera@users.noreply.github.com>
2023-04-12 19:43:01 -0300

    guadi-2

--- TP2-Simulación/Clases/Poisson.cs
+++ TP2-Simulación/Clases/Poisson.cs
@@ -8,19 +8,48 @@ namespace TP2_Simulación.Clases
 {
     internal class Poisson
     {
-        private int lambda;
+        private double lambda;
         private int datos;
 
 
-        double u;
-        double p;
-
         private Random random = new Random();
+        private List<double> rnd;
+
+        public Poisson(double lamda, int datos)
+        {
+            this.lambda = lamda;
+            this.datos = datos;
+        }
+
+        public double[] generarVariablesAleatorias()
+        {
+            double[] x = new double[this.datos];
+            List<double> rnd = new List<double>();
+            for(int i = 0; i < this.datos; i++)
+            {
+                x[i] = generarValorX();
+            }
+
+            return x;
+        }
+
+        public double generarValorX()
+        {
+            List<double> numerosAleatorios = new List<double>();
+            double p = 1;
+            double x = -1;
+            double a = Math.Exp(-(this.lambda));
+            numerosAleatorios.Add(a);
+
+            do
+            {
+                double u = Math.Truncate(random.NextDouble() * 10000) / 10000;
+                p = p * u;
+                x = x + 1;
+
+            } while (p >= a);
+            return (x);
+        }
     }
 
-
-    public void generarValorX()
-    {
-        
-    }
 }
