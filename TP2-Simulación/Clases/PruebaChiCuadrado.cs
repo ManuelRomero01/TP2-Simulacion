0ccf1c86b5b427d40662ab8dc0c826fe476372cf
Cathutu <catalinabenegas@gmail.com>
2023-04-16 11:11:59 -0300

    Prueba Chi Cuadrado terminada
    Terminé la forma en la que la función devuelve los datos. Hardcodeé los valores tabulados porque no sé si tenemos alguna función que los calcule automáticamente.
    
    Agregué una función de cálculo de factorial en Auxiliar y la usé en Cálculo Probabilidad.
    
    Eliminé "usings" (directivas) que no se usaban en estas tres clases que toqué, no revisé ninguna otra más.

--- TP2-Simulación/Clases/PruebaChiCuadrado.cs
+++ TP2-Simulación/Clases/PruebaChiCuadrado.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TP2_Simulación.Clases;
+
 namespace TP2_Simulación.Clases
 {
     internal class PruebaChiCuadrado
@@ -11,7 +8,7 @@ namespace TP2_Simulación.Clases
         private double precision = 0.0001;
         private double[][] tabla;
 
-        public (double[], double) probarChiCuadrado(int intervalos, double[] valoresAleatorios, double significancia, double variable1, double variable2, int tipoDistr)
+        public (double[][], double) probarChiCuadrado(int intervalos, double[] valoresAleatorios, double significancia, double variable1, double variable2, int tipoDistr)
         {
             // Cálculo de los datos iniciales
             double min = valoresAleatorios.Min();
@@ -29,8 +26,11 @@ namespace TP2_Simulación.Clases
             fo = 0;
             cAcum = 0;
 
+            // Variable de datos empíricos
+            int m = 0;
+
             // Creo la tabla
-            tabla = double[intervalos][7];
+            tabla = new double [intervalos][];
 
             // Hace el cálculo por intervalo
             for (int i = 0; i < intervalos; i++)
@@ -47,18 +47,26 @@ namespace TP2_Simulación.Clases
                     case 0:
                         peI = CalculoProbabilidad.probabilidadNormal(limInf, media: variable1, desviacion: variable2);
                         peS = CalculoProbabilidad.probabilidadNormal(limSup, media: variable1, desviacion: variable2);
+                        m = 2;
                         break;
                     case 1:
                         peI = CalculoProbabilidad.probabilidadUniforme(limInf, a: variable1, b: variable2);
                         peS = CalculoProbabilidad.probabilidadUniforme(limSup, a: variable1, b: variable2);
+                        m = 0;
                         break;
                     case 2:
                         peI = CalculoProbabilidad.probabilidadExponencial(limInf, lambda: variable1);
                         peS = CalculoProbabilidad.probabilidadExponencial(limSup, lambda: variable1);
+                        m = 1;
                         break;
                     case 3:
                         peI = CalculoProbabilidad.probabilidadPoisson(limInf, lambda: variable1);
                         peS = CalculoProbabilidad.probabilidadPoisson(limSup, lambda: variable1);
+                        m = 1;
+                        break;
+                    default:
+                        peI = 0;
+                        peS = 0;
                         break;
                 }
 
@@ -69,16 +77,152 @@ namespace TP2_Simulación.Clases
                 fe = cantidad * pe;
                 difFrec = Math.Pow(fo - fe, 2);
                 c = difFrec / fe;
-                cAcum += c; 
+                cAcum += c;
 
-                // HACER Guardar datos en vector
+                // Guardar datos en la celda del vector
+                double[] celda = { limInf, limSup, fo, fe, difFrec, c, cAcum }; 
+                tabla[i] = celda;
 
                 // Preparación para la siguiente vuelta
                 limInf = limInf + anchoIntervalo; 
             }
 
-            double[] x = { 0.0 }; //borrar
-            return (x, 0.0);
+            double chiTab = chiTabulado(intervalos, m, significancia);
+            
+            return (tabla, chiTab);
+        }
+        
+        public double chiTabulado(int k, int m, double significancia)
+        {
+            // Con intervalos definidos, calculo todas las posibilidades
+            // En todos los casos, el "else" es significancia de 0,025
+            double tabulado = 0;
+
+            switch (k - 1 - m)
+            {
+                case 2:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 6;
+                    }
+                    else
+                    {
+                        tabulado = 7.4;
+                    }
+                    break;
+
+                case 4:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 9.5;
+                    }
+                    else
+                    {
+                        tabulado = 11.1;
+                    }
+                    break;
+
+                case 5:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 11.1;
+                    }
+                    else
+                    {
+                        tabulado = 12.8;
+                    }
+                    break;
+
+                case 7:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 14.1;
+                    }
+                    else
+                    {
+                        tabulado = 16.0;
+                    }
+                    break;
+
+                case 8:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 15.5;
+                    }
+                    else
+                    {
+                        tabulado = 17.5;
+                    }
+                    break;
+
+                case 9:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 16.9;
+                    }
+                    else
+                    {
+                        tabulado = 19.0;
+                    }
+                    break;
+
+                case 10:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 18.3;
+                    }
+                    else
+                    {
+                        tabulado = 20.5;
+                    }
+                    break;
+
+                case 11:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 19.7;
+                    }
+                    else
+                    {
+                        tabulado = 21.9;
+                    }
+                    break;
+
+                case 12:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 21;
+                    }
+                    else
+                    {
+                        tabulado = 23.3;
+                    }
+                    break;
+
+                case 14:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 23.7;
+                    }
+                    else
+                    {
+                        tabulado = 26.1;
+                    }
+                    break;
+
+                case 15:
+                    if (significancia == 0.05)
+                    {
+                        tabulado = 25.0;
+                    }
+                    else
+                    {
+                        tabulado = 27.5;
+                    }
+                    break;
+            }
+
+            return tabulado;
         }
     }
 }
